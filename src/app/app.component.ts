b428262a2cb413e95c1379e089102fbf80e4f95e
simonaco-circle <simona_cotin@yahoo.com>
2017-11-01 12:01:48 +0000

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  functionEndpoint = `https://sicotin-hue.azurewebsites.net/api/GetToken`;
+  constructor(private http: Http) {}
+  putBlob(apiEndPoint, file) {
+    const headers = new Headers();
+    headers.append('x-ms-blob-type', 'BlockBlob');
+    headers.append('x-ms-blob-content-type', file.type);
+    const options = new RequestOptions({ headers: headers });
+    this.http
+      .put(apiEndPoint, file, options)
+      .map(res => res.json())
+      .catch(error => Observable.throw(error))
+      .subscribe(data => console.log('success'), error => console.log(error));
+  }
+  fileChange(event) {
+    const fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      const file: File = fileList[0];
+
+      this.http
+        .get(this.functionEndpoint + `?name=${file.name}`)
+        .map(res => res.json())
+        .catch(error => Observable.throw(error))
+        .subscribe(
+          data => this.putBlob(data, file),
+          error => console.log(error)
+        );
+    }
+  }
 }
